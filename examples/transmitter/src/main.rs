b49a0b8f2eee1a5694229a13f2193a2fd1f9d696
Johan <johan@jott.se>
2019-09-25 19:53:29 +0200

    Remove usb

--- examples/transmitter/src/main.rs
+++ examples/transmitter/src/main.rs
@@ -4,10 +4,8 @@
 
 use panic_semihosting as _;
 
-use cortex_m::asm::delay;
 use cortex_m_rt::entry;
 use cortex_m_semihosting::hprintln;
-use stm32_usbd::UsbBus;
 use stm32f1xx_hal::{
     gpio::{gpiob::PB9, Alternate, PushPull},
     pac,
@@ -16,11 +14,7 @@ use stm32f1xx_hal::{
     stm32::{interrupt, TIM2, TIM4},
     timer::{Event, Timer},
 };
-use usb_device::prelude::*;
-use usbd_serial::{SerialPort, USB_CLASS_CDC};
 
-use heapless::consts::*;
-use heapless::spsc::Queue;
 
 use infrared::{
     nec::remotes::*,
@@ -37,9 +31,6 @@ static mut NECTX: Option<NecSamsungTransmitter> = None;
 // Pwm channel
 static mut PWM: Option<Pwm<TIM4, C4>> = None;
 // Our remote control we want to act like
-static REMOTECONTROL: SamsungTv = SamsungTv;
-// Remote control action queue
-static mut TXQ: Option<Queue<SamsungTvAction, U8>> = None;
 
 struct PwmChannels(PB9<Alternate<PushPull>>);
 impl Pins<TIM4> for PwmChannels {
@@ -68,27 +59,6 @@ fn main() -> ! {
 
     assert!(clocks.usbclk_valid());
 
-    let mut gpioa = device.GPIOA.split(&mut rcc.apb2);
-
-    // BluePill board has a pull-up resistor on the D+ line.
-    // Pull the D+ pin down to send a RESET condition to the USB bus.
-    let mut usb_dp = gpioa.pa12.into_push_pull_output(&mut gpioa.crh);
-    usb_dp.set_low();
-    delay(clocks.sysclk().0 / 100);
-
-    let usb_dm = gpioa.pa11;
-    let usb_dp = usb_dp.into_floating_input(&mut gpioa.crh);
-
-    let usb_bus = UsbBus::new(device.USB, (usb_dm, usb_dp));
-
-    let mut serial = SerialPort::new(&usb_bus);
-
-    let mut usb_dev = UsbDeviceBuilder::new(&usb_bus, UsbVidPid(0x5824, 0x27dd))
-        .manufacturer("Fake company")
-        .product("Serial port")
-        .serial_number("TEST")
-        .device_class(USB_CLASS_CDC)
-        .build();
 
     let mut timer = Timer::tim2(device.TIM2, 20.khz(), clocks, &mut rcc.apb1);
     timer.listen(Event::Update);
@@ -119,46 +89,9 @@ fn main() -> ! {
 
     core.NVIC.enable(pac::Interrupt::TIM2);
 
-    // Initialize the queue
-    unsafe {
-        TXQ = Some(Queue::new());
-    };
-
-    let mut action_queue = unsafe { TXQ.as_mut().unwrap().split().0 };
 
     loop {
-        // Handle USB
-        if usb_dev.poll(&mut [&mut serial]) {
-            let mut buf = [0u8; 16];
-
-            match serial.read(&mut buf) {
-                Ok(count) if count > 0 => {
-                    for c in buf[0..count].iter() {
-                        // Keyboard key to Remote control action
-                        let action = match *c as char {
-                            'o' => SamsungTvAction::Power,
-                            'n' => SamsungTvAction::ChannelListNext,
-                            'p' => SamsungTvAction::ChannelListPrev,
-                            _ => SamsungTvAction::Teletext,
-                        };
-
-                        // Queue the action
-                        action_queue.enqueue(action).ok().unwrap();
-                    }
-
-                    let mut write_offset = 0;
-                    while write_offset < count {
-                        match serial.write(&buf[write_offset..count]) {
-                            Ok(len) if len > 0 => {
-                                write_offset += len;
-                            }
-                            _ => {}
-                        }
-                    }
-                }
-                _ => {}
-            }
-        }
+        continue;
     }
 }
 
