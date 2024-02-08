de3cd1f116df477222abc616dd468d32af1d8314
Johan Kristell <johan@oribi.se>
2024-02-08 20:40:53 +0100

    Update toolchain and dependencies to make the examples build again

--- examples/arduino_uno/src/bin/external-interrupt.rs
+++ examples/arduino_uno/src/bin/external-interrupt.rs
@@ -79,7 +79,7 @@ fn main() -> ! {
     // Enable interrupts globally
     unsafe { avr_device::interrupt::enable() };
 
-    ufmt::uwriteln!(&mut serial, "Hello from Arduino and Irdroino!\r").void_unwrap();
+    ufmt::uwriteln!(&mut serial, "Hello from Arduino and Irdroino!\r").unwrap_infallible();
 
     loop {
         if let Some(cmd) = avr_device::interrupt::free(|cs| CMD.borrow(cs).take()) {
@@ -90,7 +90,7 @@ fn main() -> ! {
                 cmd.cmd,
                 cmd.repeat
             )
-            .void_unwrap();
+            .unwrap_infallible();
         }
 
         arduino_hal::delay_ms(100);
